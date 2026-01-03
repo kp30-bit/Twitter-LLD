c256c9f2ffb9a3d140221728f9df246b4849c855
Kamal Pratik <kamalpratik144@gmail.com>
2026-01-03 17:23:51 +0530

    Added comments to justify use of solid principle

--- internal/interfaces/Ifeedstrategy.go
+++ internal/interfaces/Ifeedstrategy.go
@@ -2,6 +2,6 @@ package interfaces
 
 import "twitter-lld/internal/domain"
 
-type ILoadFeedStrategy interface {
-	LoadFeed() []*domain.Tweet
+type ILoadFeedStrategy interface { //Dependency Infversion principle :High-level modules should not depend on low-level modules. Both should depend on abstractions
+	LoadFeed() []*domain.Tweet //Implemented interface segragation principle because we are just exposing the method that is needed.
 }
--- internal/interfaces/Itweetservice.go
+++ internal/interfaces/Itweetservice.go
@@ -2,6 +2,6 @@ package interfaces
 
 import "twitter-lld/internal/domain"
 
-type TweetService interface {
-	GetTweetMap() map[int]*domain.Tweet
+type TweetService interface { //Dependency Inversion principle :High-level modules should not depend on low-level modules. Both should depend on abstractions
+	GetTweetMap() map[int]*domain.Tweet //Implemented interface segragation principle because we are just exposing the method that is needed.
 }
